590299757f368b1b73dcaa3184caae607d1f2610
parvez13913 <parvezz13913@gmail.com>
2022-03-05 00:02:02 +0600

    javascript code compite

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ const apiKey = `2a9408a9cb0dc4f8a459a057a6ae2218`;
 const searchTemperature = () => {
     const cityinput = document.getElementById('city-name');
     const cityText =  cityinput.value;
+    if(cityText === ''){
+        return;
+    }
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityText}&appid=${apiKey}&units=metric`;
     fetch(url)
     .then(res => res.json())
@@ -13,8 +16,10 @@ const setInnerText = (id,text) => {
 }
 
 const displayData = data => {
-    console.log(data);
     setInnerText('city',data.name);
     setInnerText('temperature',data.main.temp);
     setInnerText('condition',data.weather[0].main);
+    const url = ` http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const imagesIcon = document.getElementById('weather-icon');
+    imagesIcon.setAttribute('src',url);
 }
